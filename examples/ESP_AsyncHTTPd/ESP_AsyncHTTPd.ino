ef3fa2eaa861c0b6fc7b75e35122e139a0252e26
Zhenyu Wu <Adam_5Wu@hotmail.com>
2017-02-03 22:32:37 -0500

    Adjust example to compile with new implementation

--- examples/ESP_AsyncHTTPd/ESP_AsyncHTTPd.ino
+++ examples/ESP_AsyncHTTPd/ESP_AsyncHTTPd.ino
@@ -68,62 +68,47 @@ void setup() {
   Serial.printf("FATFS: %d total, %d used (%.1f%%), block size %d\n",
                 info.totalBytes, info.usedBytes, info.usedBytes*100.0/info.totalBytes, info.blockSize);
 
-  wwwSrv.on("/all", HTTP_GET, [](AsyncWebServerRequest *request){
+  wwwSrv.on("/all", HTTP_GET, [](AsyncWebRequest &request){
     String json = "{";
     json += "\"heap\":"+String(ESP.getFreeHeap());
     json += ", \"analog\":"+String(analogRead(A0));
     json += ", \"gpio\":"+String((uint32_t)(((GPI | GPO) & 0xFFFF) | ((GP16I & 0x01) << 16)));
     json += "}";
-    request->send(200, json, "text/json");
+    request.send(200, json, "text/json");
     json = String();
   });
 
-  wwwSrv.serveStatic("/", VFATFS.openDir("/"));
+  wwwSrv.serveStatic("/test", VFATFS.openDir("/"));
 
-  wwwSrv.catchAll([](AsyncWebServerRequest *request){
+  wwwSrv.catchAll([](AsyncWebRequest &request){
     Serial.print("NO_HANDLER: ");
-    if(request->method() == HTTP_GET)
-      Serial.print("GET");
-    else if(request->method() == HTTP_POST)
-      Serial.print("POST");
-    else if(request->method() == HTTP_DELETE)
-      Serial.print("DELETE");
-    else if(request->method() == HTTP_PUT)
-      Serial.print("PUT");
-    else if(request->method() == HTTP_PATCH)
-      Serial.print("PATCH");
-    else if(request->method() == HTTP_HEAD)
-      Serial.print("HEAD");
-    else if(request->method() == HTTP_OPTIONS)
-      Serial.print("OPTIONS");
-    else
-      Serial.print("UNKNOWN");
-    Serial.printf(" http://%s%s\n", request->host().c_str(), request->url().c_str());
+    Serial.print(request.methodToString());
+    Serial.printf(" %s %s\n", request.host().c_str(), request.url().c_str());
 
-    if(request->contentLength()){
-      Serial.printf("_CONTENT_TYPE: %s\n", request->contentType().c_str());
-      Serial.printf("_CONTENT_LENGTH: %u\n", request->contentLength());
+    if(request.contentLength()){
+      Serial.printf("_CONTENT_TYPE: %s\n", request.contentType().c_str());
+      Serial.printf("_CONTENT_LENGTH: %u\n", request.contentLength());
     }
 
-    int headers = request->headers();
-    int i;
-    for(i=0;i<headers;i++){
-      AsyncWebHeader* h = request->getHeader(i);
-      Serial.printf("_HEADER[%s]: %s\n", h->name().c_str(), h->value().c_str());
-    }
+    request.enumQueries([&](AsyncWebQuery const& q){
+      Serial.printf("_QUERY[%s]: %s\n", q.name.c_str(), q.value.c_str());
+      return false;
+    });
 
-    int params = request->params();
-    for(i=0;i<params;i++){
-      AsyncWebParameter* p = request->getParam(i);
-      if(p->isFile()){
-        Serial.printf("_FILE[%s]: %s, size: %u\n", p->name().c_str(), p->value().c_str(), p->size());
-      } else if(p->isPost()){
-        Serial.printf("_POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
-      } else {
-        Serial.printf("_GET[%s]: %s\n", p->name().c_str(), p->value().c_str());
+    request.enumHeaders([&](AsyncWebHeader const& h){
+      Serial.printf("_HEADER[%s]:",h.name.c_str());
+      String values;
+      for (auto v : h.values) {
+        values.concat(' ');
+        values.concat(v);
+        values.concat(',');
       }
-    }
-    request->send(404);
+      values.remove(values.length()-1);
+      Serial.println(values);
+      return false;
+    });
+
+    request.send(404);
   });
 
   wwwSrv.begin();
